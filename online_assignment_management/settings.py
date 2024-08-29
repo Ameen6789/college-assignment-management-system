1ea7b0e6d513c99ab2783e01881879f223ada4e6
Azarath Ameen C A <70131572+Ameen6789@users.noreply.github.com>
2024-08-29 22:44:08 +0530

    Update settings.py

--- online_assignment_management/settings.py
+++ online_assignment_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3urk!w82ry*z#oj(@ny2e1j=k1sr66ll#z#8$ko(cc%$$r8iy6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app"]
 
