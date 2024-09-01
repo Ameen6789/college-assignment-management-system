fbdc3b1b753e7c3d02673c6b5d759d90ff464d75
ameen6789 <azrucha@gmail.com>
2024-09-02 00:20:09 +0530

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 python3.12 -m venv myenv
-source myenv/Scripts/activate
+source myenv/bin/activate
 pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
 
