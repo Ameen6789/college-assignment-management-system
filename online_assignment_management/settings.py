a9be6546e802e68e3e2c6595a655d52349a8836e
ameen6789 <azrucha@gmail.com>
2024-09-01 17:13:49 +0530

    changes

--- online_assignment_management/settings.py
+++ online_assignment_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3urk!w82ry*z#oj(@ny2e1j=k1sr66ll#z#8$ko(cc%$$r8iy6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app",".now.sh","127.0.0.1","localhost"]
+ALLOWED_HOSTS = [".vercel.app",".now.sh","127.0.0.1"]
 
 
 # Application definition
