ceb79b8924829a8e8b3e5030c93128bbc72861e9
Azarath Ameen C A <70131572+Ameen6789@users.noreply.github.com>
2025-09-28 22:28:58 +0530

    Update settings.py

--- online_assignment_management/settings.py
+++ online_assignment_management/settings.py
@@ -393,10 +393,10 @@ LOGIN_URL="/"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-AWS_ACCESS_KEY_ID = ""
-AWS_SECRET_ACCESS_KEY = ""
-AWS_STORAGE_BUCKET_NAME = ""
-AWS_S3_ENDPOINT_URL = ""
+AWS_ACCESS_KEY_ID = "1"
+AWS_SECRET_ACCESS_KEY = "1"
+AWS_STORAGE_BUCKET_NAME = "1"
+AWS_S3_ENDPOINT_URL = "1"
 
 AWS_S3_REGION_NAME = "ap-south-1"   # (or whatever Supabase region you created the project in)
 AWS_DEFAULT_ACL = None 
